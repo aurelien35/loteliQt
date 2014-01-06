bdbdf820f0dace10a6c12bf187eec5e748afe72c
Aurelien ALBERT <aurelien.albert@alyotech.fr>
2014-01-06 12:00:45 +0100

    StringConvert

---
+++ sources/Tools/StringConvert.py
@@ -0,0 +1,15 @@
+def str2QString(text) :
+	if (text == None) :
+		return unicode("", 'utf-8')
+	return unicode(text, 'utf-8')
+	
+def QString2str(text) :
+	return str(text.toUtf8())
+	
+def date2str(date) :
+	if (date == None) :
+		return ""
+	return date.strftime('%d/%m/%Y')
+	
+def date2QString(date) :
+	return str2QString(date2str(date))
